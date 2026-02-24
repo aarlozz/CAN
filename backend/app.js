2263ba510817c30017128f0f83e88e2f86ade7a1
premalshrestha <premalshrestha00@gmail.com>
2026-02-24 21:28:49 +0545

    Edited to add instutional url

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth",authRoutes);
-app.use("/api/user",userRoutes);
+app.use("/api/institutional",userRoutes);
 
 // Test route
 app.get("/", (req, res) => {

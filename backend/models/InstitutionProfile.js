c9031eaab73e53243bc48387235a9ec68e521b01
premalshrestha <premalshrestha00@gmail.com>
2026-03-29 22:23:26 +0545

    Added status and reomved isApproved variable in model of institution profile

--- backend/models/InstitutionProfile.js
+++ backend/models/InstitutionProfile.js
@@ -1,69 +1,72 @@
 import mongoose from "mongoose";
 
-const institutionSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-    unique: true,
-  },
+const institutionSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
 
-  institutionName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  institutionType: {
-    type: String,
-    required: true,
-    enum: ["School", "College", "University"],
-  },
-  establishedYear: {
-    type: Number,
-  },
-  location: {
-    province: {
+    institutionName: {
       type: String,
       required: true,
+      trim: true,
     },
-    district: {
+    institutionType: {
       type: String,
       required: true,
+      enum: ["School", "College", "University"],
     },
-    municipality: {
+    establishedYear: {
+      type: Number,
+    },
+    location: {
+      province: {
+        type: String,
+        required: true,
+      },
+      district: {
+        type: String,
+        required: true,
+      },
+      municipality: {
+        type: String,
+      },
+      ward: {
+        type: String,
+      },
+      street: {
+        type: String,
+      },
+    },
+
+    website: {
       type: String,
     },
-    ward: {
+    description: {
       type: String,
     },
-    street: {
+
+    contactPerson: {
+      name: String,
+      phone: String,
+      email: String,
+      designation: String,
+    },
+
+    status: {
       type: String,
+      enum: ["approved", "pending", "denied"],
+      default: "pending",
     },
   },
+  { timestamps: true },
+);
 
-  website: {
-    type: String,
-  },
-  description: {
-    type: String,
-  },
+institutionSchema.index({ "location.province": 1 });
+institutionSchema.index({ "location.district": 1 });
+institutionSchema.index({ institutionName: 1 });
 
-  contactPerson: {
-    name: String,
-    phone: String,
-    email: String,
-    designation: String,
-  },
-
-  isApproved:{
-    type: Boolean,
-    default: false
-  },
-},
-{ timestamps: true});
-
-institutionSchema.index({"location.province": 1})
-institutionSchema.index({"location.district": 1})
-institutionSchema.index({"institutionName": 1})
-
-export default mongoose.model("InstitutionProfile", institutionSchema)
+export default mongoose.model("InstitutionProfile", institutionSchema);

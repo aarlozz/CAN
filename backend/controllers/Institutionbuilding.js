1a919a99b5afb342874ed122a92f636161f01db9
aarlozz <kiransilwal108@gmail.com>
2026-03-30 11:08:49 +0545

    Resolved merge conflicts in Institutionbuilding controller

--- backend/controllers/Institutionbuilding.js
+++ backend/controllers/Institutionbuilding.js
@@ -1,8 +1,7 @@
 import InstitutionProfile from "../models/InstitutionProfile.js";
 import Application from "../models/Application.js";
 
-<<<<<<< HEAD
-
+// Get all institutions (public)
 export const getAllInstitutions = async (req, res) => {
   try {
     const institutions = await InstitutionProfile
@@ -13,20 +12,21 @@ export const getAllInstitutions = async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
-=======
+  }
+};
+
+// Get institution data for logged-in user (protected)
 export const getInstitutionData = async (req, res) => {
   try {
     const institution = await InstitutionProfile.findOne({
       user: req.user.id,
     }).populate("user", "name email");
-    //or we can use .select("iinstitutuionName institutionType") rakhda ni hunxa muni ko nagari kana
+
     if (!institution) {
       return res.status(404).json({ message: "Institution not found" });
     }
-    //esle chai whole data nai pathauxa
-    //hamle esma insituton ko individual data like
-    // res.json({ institutionName: institution.institutuionName, institutionType: institution.institutionType})
-    // vanera aafnai chailyeko data matra lida hunxa
+
+    // Send full institution data; can customize to send only required fields
     res.json({ institution });
   } catch (error) {
     console.error(error);
@@ -34,46 +34,28 @@ export const getInstitutionData = async (req, res) => {
   }
 };
 
+// Get application data related to logged-in institution (protected)
 export const getApplicationData = async (req, res) => {
   try {
-    //suru ma student le apply garnu vanda aagadi institution ko data liyo
-    const insititution = await InstitutionProfile.findOne({
+    const institution = await InstitutionProfile.findOne({
       user: req.user.id,
     });
-    //ani tesma hamle application ma jasle jasle thicha tesko institution id khojera lyako
-    const application = await Application.findOne({
-      insititution: insititution._id,
-      //student ko remaining data aaba lyaera halney
-    }).populate("student");
-    //ani frontend ko ma pathaune
 
-    res.json(application);
+    if (!institution) {
+      return res.status(404).json({ message: "Institution not found" });
+    }
+
+    const application = await Application.findOne({
+      institution: institution._id,
+    }).populate("student"); // populate student info
+
+    if (!application) {
+      return res.status(404).json({ message: "Application not found" });
+    }
+
+    res.json({ application });
   } catch (error) {
-    return res.status(400).json({
-      message: console.message,
-    });
->>>>>>> kiran
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
-
-
-// export const getInstitutionData = async (req, res) => {
-
-//     try{
-//   const institution = await InstitutionProfile.findOne({ user: req.user.id }).populate("user","name email");
-//   //or we can use .select("iinstitutuionName institutionType") rakhda ni hunxa muni ko nagari kana
-//   if(!institution){
-//     return res.status(404).json({message:"Institution not found"})
-//   }
-// //esle chai whole data nai pathauxa
-// //hamle esma insituton ko individual data like
-// // res.json({ institutionName: institution.institutuionName, institutionType: institution.institutionType})
-// // vanera aafnai chailyeko data matra lida hunxa
-//   res.json({institution})
-// }
-// catch(error){
-//     console.error(error)
-//     return res.status(500).json({message:"Internal server error"})
-        
-// }
-// };

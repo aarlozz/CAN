54124021df977052670fd93a0110d778eb723fbd
premalshrestha <premalshrestha00@gmail.com>
2026-03-30 11:42:10 +0545

    Resolved merge error

--- backend/routes/InstitutionbuildingRoutes.js
+++ backend/routes/InstitutionbuildingRoutes.js
@@ -1,14 +1,8 @@
 import { getAllInstitutions } from "../controllers/Institutionbuilding.js";
 import express from "express";
-<<<<<<< HEAD
-
-const router = express.Router();
-
-router.get("/all-institution", getAllInstitutions);
-export default router
-=======
 import { protect } from "../middleware/authMiddleware.js";
 import { getApplicationData } from "../controllers/Institutionbuilding.js";
+import { getInstitutionData } from "../controllers/Institutionbuilding.js";
 
 const router = express.Router();
 
@@ -16,4 +10,3 @@ router.get("/all-institution", protect, getInstitutionData);
 router.get("/getapplicationdata", protect, getApplicationData);
 
 export default router;
->>>>>>> kiran
